b08e08ffcbd0528474b4f97f1661e2ac84fe39ce
ruscoder <rusmiligamer@gmail.com>
2014-03-28 16:05:57 +0800

    fix

---
+++ __local.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'ir4y'
+
+USERNAME = ''
+PASSWORD = ''
+HOST = '127.0.0.1'
+
+
+RSA_PATH = "/home/user/.ssh/id_rsa.pub"
+SSH_HOST = "192.168.0.100"
+SSH_HOST_USER = "user"
+SSH_HOST_PORT = "5555"
+

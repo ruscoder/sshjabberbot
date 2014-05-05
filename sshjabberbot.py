2e098d9f657c3530dc7f259a9275cecb43c886a1
ruscoder <rusmiligamer@gmail.com>
2014-05-05 17:03:38 +0800

    changed. refactored. added module

--- sshjabberbot.py
+++ sshjabberbot.py
@@ -1,49 +1,13 @@
 # -*- coding: utf-8 -*-
-from time import time
-from twisted.words.protocols.jabber import xmlstream, client, jid
+from twisted.words.protocols.jabber import jid
 from twisted.words.xish import domish
 from functions import handle
+from advanced_jabberclient.jabberclient import jabberClientFactory
+
 import settings
-__author__ = 'ir4y'
 
 
-class PingXmlStream(xmlstream.XmlStream):
-    def __init__(self, authenticator):
-        xmlstream.XmlStream.__init__(self, authenticator)
-        self.ping_received_time = time()
-
-    def checkPong(self):
-        self.sendPing()
-        if time() - self.ping_received_time >= settings.PING_FAIL_TIME:
-            self.transport.loseConnection()
-        else:            
-            self._callLater(settings.PING_TIMEOUT, self.checkPong)
-
-    def sendPing(self):
-        msg = domish.Element(("jabber:client", "message"))
-        msg["to"] = "echo.{0}".format(settings.HOST)
-        msg.addElement("body", content="ping")
-        self.send(msg)
-
-    def receivePing(self):
-        self.ping_received_time = time()
-
-    def connectionMade(self):
-        xmlstream.XmlStream.connectionMade(self)
-        self._callLater(settings.PING_TIMEOUT, self.checkPong)
-
-
-class ReconnectClientFactory(xmlstream.XmlStreamFactory):
-    protocol = PingXmlStream
-
-    def __init__(self, authenticator):
-        xmlstream.XmlStreamFactory.__init__(self, authenticator)
-        self.maxDelay = 10
-
-
-def jabberClientFactory(jid, secret):
-    a = client.BasicAuthenticator(jid, secret)
-    return ReconnectClientFactory(a)
+__author__ = 'badim'
 
 
 def init_factory():
@@ -61,16 +25,17 @@ def init_factory():
                     else:
                         answer = handle(command)
 
-            if answer and message["from"] != message["to"]:
-                msg = domish.Element(("jabber:client", "message"))
-                msg["to"] = message["from"]
-                msg.addElement("body", content=answer)
-                xmlstream.send(msg)
+            if answer:
+                xmlstream.sendMessage(message["from"], answer)
 
         xmlstream.addObserver('/message',  gotMessage)
 
+    factory = jabberClientFactory(jid=jid.JID(settings.USERNAME),
+                                  secret=settings.PASSWORD,
+                                  ping_timeout=settings.PING_TIMEOUT,
+                                  ping_fail_time=settings.PING_FAIL_TIME,
+                                  host=settings.HOST)
 
-    factory = jabberClientFactory(jid.JID(settings.USERNAME), settings.PASSWORD)
     factory.addBootstrap('//event/stream/authd', authd)
     return factory
 
